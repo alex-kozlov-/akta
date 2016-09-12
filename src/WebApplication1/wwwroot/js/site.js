fc4bc303980aa9bf0b98cec4808c68491a1daed0
Pavel Hizhuk <pavel.hizhuk@outlook.com>
2016-09-12 11:06:50 +0300

    MVC 6 start

--- src/WebApplication1/wwwroot/js/site.js
+++ src/WebApplication1/wwwroot/js/site.js
@@ -1,2 +1,15 @@
-﻿var elem = document.getElementById("username");
-elem.innerHTML = "Alexander Kozlov"
+﻿(function () {
+
+    var $sidebarAndWrapper = $('#sidebar, #wrapper');
+
+    $('#toggleSidebar').on('click', function () {
+
+        $sidebarAndWrapper.toggleClass('hide-sidebar');
+
+        if ($sidebarAndWrapper.hasClass('hide-sidebar')) {
+            $(this).text("Show Toggle");
+        } else {
+            $(this).text("Hide Toggle");
+        }
+    })
+})()
